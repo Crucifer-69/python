391b1e5d7796d23f57a97e8ce299447d5a23a14a
sanketb03 <sanketb03@gmail.com>
2019-11-13 13:58:56 +0530

    varaibles and Printing

--- Pyhon exercises/ex5.py
+++ Pyhon exercises/ex5.py
@@ -1,4 +1,4 @@
-my_name = 'Zed A. shaw'
+ my_name = 'Zed A. shaw'
 my_age = 35 #not a lie
 my_height = 74 #inches
 my_weight = 180 #lbs
@@ -7,7 +7,7 @@ my_teeth = 'White'
 my_hair = 'brown'
 
 print("Let's talk about %s." % my_name)
-print("He's %d inches tall." % my_height)
+print("He's %d inches tall." % my_height)   
 print("he's %d pounds heavy" % my_weight)
 print("Actually thats not too heavy")
 print("He's got %s eyes and %s hair"% (my_eyes, my_hair))

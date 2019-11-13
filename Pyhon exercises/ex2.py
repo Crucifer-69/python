d33c38de1275b98f9cb5d004ac29e4a3942e59df
sanketb03 <sanketb03@gmail.com>
2019-11-13 13:57:18 +0530

    comments and pound characters

--- Pyhon exercises/ex2.py
+++ Pyhon exercises/ex2.py
@@ -6,4 +6,4 @@ print("I could have code like this.") #and the comment after is ignored
 #You also use the comment to "disable"  or comment out a piece of code.
 #print("This won't run")
 
-print("This will run")  
+print("This will run")    

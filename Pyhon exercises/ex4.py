19396a4f924a19cd25e98f0ae62b9ca11091fa1f
sanketb03 <sanketb03@gmail.com>
2019-11-13 13:58:20 +0530

    Variables and names

--- Pyhon exercises/ex4.py
+++ Pyhon exercises/ex4.py
@@ -7,7 +7,7 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passenger_per_car = passengers / cars_driven
 
-print("There are ", cars, " cars avaliable")
+print("There are ", cars, " cars avaliable")    
 print("There are only ", drivers," drivers avaliable") 
 print("There will be ", cars_ont_driven," empty cars avaliable")
 print("We can transport ", carpool_capacity," people today")

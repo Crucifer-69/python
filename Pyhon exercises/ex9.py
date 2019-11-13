4b71662b271868555281eeedcc5c867bef29d29d
sanketb03 <sanketb03@gmail.com>
2019-11-13 18:38:03 +0530

    multi-line printing with three double-quotes

---
+++ Pyhon exercises/ex9.py
@@ -0,0 +1,12 @@
+days  = "Mon	Tue		Wed		Thu		Fri		Sat		Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:",days)
+print("Here are the months: ", months)
+
+print("""
+	Ther's something going on here.
+	with the three doule-qoutes.
+	we will be able to type as much as we like.
+	Even on 4 line if we want, or 5 or 6.
+	""")

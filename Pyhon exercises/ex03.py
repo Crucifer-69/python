5dadb661cbb4fbe74380e605542f582709a27702
sanketb03 <sanketb03@gmail.com>
2019-11-17 14:09:12 +0530

    Numbers and math

---
+++ Pyhon exercises/ex03.py
@@ -0,0 +1,20 @@
+print("I wiil now count my chickens:")
+
+print("Hens ", 25+30/6)
+print("Roosters ", 100-25*3%4)
+
+print("Now I wiil count eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("It is true that 3+2 < 5-7")
+print(3+2 < 5-7)
+
+print("What is 3+2? ",3+2)
+
+print("What is 5-7? ",5-7)
+print("That's why it's false.")
+
+print("It is greater? ", 5>-2)   
+print("It is greater or equal?", 5>=2)
+print("It is less than or equal?", 5<=-2)   

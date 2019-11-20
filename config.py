a4e9df24bf15d5eb33bf1f4ac321521b692081e3
sanketb03 <sanketb03@gmail.com>
2019-11-20 18:58:20 +0530

    contains email and password. Goes with smtp_mail.py

---
+++ config.py
@@ -0,0 +1,15 @@
+"""smtp_mail.py file and config.py file needs to be in the same directory 
+	
+"""
+
+#email and password fo sender is necessary
+SENDER_EMAIL = ""
+PASSWORD = ""
+
+#only email on receivers is required
+RECEIVER_EMAIL = ""
+
+#enable access for less secure apps on gmail from the below link
+#if not enabled email won't be sent
+
+#https://myaccount.google.com/lesssecureapps

046b15e72860e880fe25944ac8b4490e72a78906
sanket <30651104+sanketb03@users.noreply.github.com>
2019-12-22 21:53:47 +0530

    Update img_to_text.py

--- img_to_text.py
+++ img_to_text.py
@@ -1,6 +1,9 @@
+#needs pytesseract to be installed on the system
+
 import pytesseract
 from PIL import Image
 
+#path to pytesseract on the system(compusary)
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 print("\nKeep the program and photo in the same file\n")
 image = input("Enter the exact name of the image: ")

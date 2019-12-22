937e9349363643d47709b5edda77cd87bd1f4929
sanketb03 <sanketb03@gmail.com>
2019-12-22 21:47:10 +0530

    converts image to text and stores them in a file form

---
+++ img_to_text.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+print("\nKeep the program and photo in the same file\n")
+image = input("Enter the exact name of the image: ")
+img = Image.open(image)
+text1 = pytesseract.image_to_string(img)
+
+txt = open('text.txt', 'a')
+
+txt.write('\n\n\n'+text1 )
+
+txt.close()
+
+print("Operation complete")
